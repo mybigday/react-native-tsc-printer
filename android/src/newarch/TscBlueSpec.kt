870b6abbf53656539d36f2fd377a9238e22923d1
Hans <me@hans00.me>
2025-06-09 12:38:53 +0800

    fix: remove unused code

--- android/src/newarch/TscBlueSpec.kt
+++
@@ -1,7 +0,0 @@
-package com.tscprinter
-
-import com.facebook.react.bridge.ReactApplicationContext
-
-abstract class TscBlueSpec internal constructor(context: ReactApplicationContext) :
-  NativeTscBlueSpec(context) {
-}
